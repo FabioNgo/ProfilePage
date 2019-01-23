5901e3255e0aa54e3633cc64ee9235218acc2952
FabioNgo <ngolebaotrung@gmail.com>
2019-01-24 00:29:15 +0800

    add  profile picture

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.svg';
 import './App.css';
 import Route from "react-router-dom/es/Route";
 import Home from "./Home";
--- src/ProfilePicture.js
+++ src/ProfilePicture.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.svg';
 import './App.css';
 
 class ProfilePicture extends Component {
   render() {
     return (
-      <div className="profile-picture">
-
+      <div className="profile-picture-holder">
+        <div className="profile-picture"> </div>
       </div>
     );
   }
